31e0e71759a4a7d8140efc072ad5507437cf8151
Jesse Ogburn <jogburn88@gmail.com>
2024-12-18 06:06:03 -0500

    Add doMath fucntion for the basic four math operators

--- calculator.js
+++ calculator.js
@@ -1,5 +1,16 @@
 "use strict";
 
-const doMath = function () {
-  return 3;
+export const doMath = function (num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    default:
+      return "Unrecognized Operator";
+  }
 };
--- calculator.test.js
+++ calculator.test.js
@@ -2,7 +2,18 @@
 import { expect, test } from "vitest";
 import * as calc from "./calculator.js";
 
-test("should return 9 given 4, 5 and '+'"),
-  () => {
-    expect(calc.doMath(4, "+", 5)).toBe(9);
-  };
+test("should return 9 given 4, 5 and '+'", () => {
+  expect(calc.doMath(4, "+", 5)).toBe(9);
+});
+
+test("should return -1 given 4, 5 and '-'", () => {
+  expect(calc.doMath(4, "-", 5)).toBe(-1);
+});
+
+test("should return 20 given 4, 5 and '*'", () => {
+  expect(calc.doMath(4, "*", 5)).toBe(20);
+});
+
+test("should return 4 given 20, 5 and '/'", () => {
+  expect(calc.doMath(20, "/", 5)).toBe(4);
+});
